1d57f5fff8a25ca6ab21d07ae10a6e1f28337213
Jon-Felipe <rodriguesjf7@gmail.com>
2024-06-12 15:39:12 +0100

    header component and installed material icons

---
+++ src/components/Header.tsx
@@ -0,0 +1,98 @@
+import { useState } from 'react';
+
+// mui components
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import Badge from '@mui/material/Badge';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import FavoriteBorder from '@mui/icons-material/FavoriteBorder';
+
+const pages = ['Mens', 'Women', 'Kids'];
+const settings = ['Profile', 'Account', 'Logout'];
+
+type Props = {};
+
+function Header({}: Props) {
+  const [profileEl, setProfileEl] = useState<null | HTMLElement>(null);
+
+  const handleOnProfileClick = (event: React.MouseEvent<HTMLElement>) => {
+    setProfileEl(event.currentTarget);
+  };
+
+  return (
+    <AppBar elevation={0} color='inherit'>
+      <Container maxWidth='lg'>
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: 'flex' }}>
+            <Typography
+              variant='h6'
+              noWrap
+              component='a'
+              fontWeight={600}
+              marginRight={4}
+            >
+              Drip Shop
+            </Typography>
+            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
+              {pages.map((page) => (
+                <Button key={page} color='inherit' sx={{ display: 'block' }}>
+                  {page}
+                </Button>
+              ))}
+            </Box>
+          </Box>
+
+          <Box sx={{ flexGrow: 0, display: 'flex' }}>
+            <IconButton size='large' color='inherit'>
+              <FavoriteBorder />
+            </IconButton>
+            <IconButton size='large' color='inherit'>
+              <Badge badgeContent={0} color='error' showZero={true}>
+                <ShoppingBagOutlinedIcon />
+              </Badge>
+            </IconButton>
+            <IconButton
+              size='large'
+              color='inherit'
+              onClick={handleOnProfileClick}
+            >
+              <PersonOutlineOutlinedIcon />
+            </IconButton>
+            <Menu
+              sx={{ mt: '45px' }}
+              id='menu-appbar'
+              anchorEl={profileEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(profileEl)}
+              onClose={() => setProfileEl(null)}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting}>
+                  <Typography textAlign='center'>{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+
+export default Header;
