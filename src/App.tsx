3873807e026b68b5429f1bd9fc96e897c051c4c5
Jon-Felipe <rodriguesjf7@gmail.com>
2024-06-13 20:14:39 +0100

    cart page cart items table

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 // mui componet
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 // components
 import Header from './components/Header';
@@ -10,8 +11,10 @@ function App() {
   return (
     <>
       <Header />
-      <Box maxWidth='lg' margin='auto' marginTop={4} component='main'>
-        <Outlet />
+      <Box marginTop={4} component='main'>
+        <Container maxWidth='lg'>
+          <Outlet />
+        </Container>
       </Box>
     </>
   );
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,5 +1,111 @@
+import { useAppSelector } from '../hooks/hooks';
+
+// mui components
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+
 function Cart() {
-  return <div>Cart</div>;
+  const { cartItems } = useAppSelector((store) => store.cart);
+
+  if (cartItems.length === 0) {
+    return (
+      <Box marginTop='80px'>
+        <Typography align='center' component='h4' variant='h5'>
+          No items found in your cart.
+        </Typography>
+      </Box>
+    );
+  }
+
+  return (
+    <Grid container spacing={2}>
+      {/* cart items */}
+      <Grid item xs={12} sm={8} component='section'>
+        <TableContainer>
+          <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+            <TableHead>
+              <TableRow>
+                <TableCell sx={{ fontWeight: 'bold' }}>Product</TableCell>
+                <TableCell align='right' sx={{ fontWeight: 'bold' }}>
+                  Price
+                </TableCell>
+                <TableCell align='right' sx={{ fontWeight: 'bold' }}>
+                  Quantity
+                </TableCell>
+                <TableCell align='right' sx={{ fontWeight: 'bold' }}>
+                  Total
+                </TableCell>
+                <TableCell
+                  align='right'
+                  sx={{ fontWeight: 'bold' }}
+                ></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {cartItems.map((cartItem, index) => {
+                return (
+                  <TableRow
+                    key={index}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                  >
+                    <TableCell component='th' scope='row'>
+                      <Box
+                        sx={{
+                          display: { xs: 'block', sm: 'flex' },
+                          alignItems: 'center',
+                          columnGap: '10px',
+                        }}
+                      >
+                        <Box
+                          component='img'
+                          src='../../images/mens_shirt.jpg'
+                          maxWidth={100}
+                          maxHeight={100}
+                          sx={{
+                            display: { xs: 'none', md: 'inline-block' },
+                            objectFit: 'cover',
+                          }}
+                        />
+                        <Box>
+                          <Typography>{cartItem.product.title}</Typography>
+                        </Box>
+                      </Box>
+                    </TableCell>
+                    <TableCell align='right'>
+                      ${cartItem.product.price}
+                    </TableCell>
+                    <TableCell align='right'>{cartItem.quantity}</TableCell>
+                    <TableCell align='right'>
+                      $
+                      {(cartItem.product.price! * cartItem.quantity).toFixed(2)}
+                    </TableCell>
+                    <TableCell align='right'>
+                      <IconButton color='error'>
+                        <DeleteIcon />
+                      </IconButton>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </Grid>
+      {/* cart summary */}
+      <Grid item xs={12} sm={4} component='section'>
+        cart summary
+      </Grid>
+    </Grid>
+  );
 }
 
 export default Cart;
