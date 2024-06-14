048bb6d6f2401856de103edb593db092e06b560f
Jon-Felipe <rodriguesjf7@gmail.com>
2024-06-14 10:10:13 +0100

    home screen deparment image links

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -15,58 +15,101 @@ import { dummy_products } from '../utils/constants';
 
 function Home() {
   return (
-    <Grid container spacing={3}>
-      {dummy_products.map((product, index) => {
-        return (
-          <Grid key={index} item xs={12} sm={6} md={4} lg={2}>
-            <Card
-              elevation={0}
-              component={Link}
-              to={`products/${product.id}`}
-              sx={{ textDecoration: 'none' }}
-            >
-              <CardMedia
-                sx={{ height: 300 }}
-                image='../images/mens_shirt.jpg'
-                title={product.title}
-              />
-              <CardContent sx={{ padding: '5px' }}>
-                <Typography
-                  component='h3'
-                  variant='h6'
-                  noWrap
-                  sx={{ fontWeight: 'bold', fontSize: '14px' }}
-                >
-                  {product.title} - {product.productDetails.color}
-                </Typography>
-                <Typography
-                  component='p'
-                  variant='body2'
-                  sx={{ color: '#888', fontSize: '12px' }}
-                >
-                  By {product.productDetails.brand}
-                </Typography>
-                <Box
-                  component='div'
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'space-between',
-                  }}
-                >
-                  <Typography component='p' sx={{ fontSize: '16px' }}>
-                    ${product.price.toFixed(2)}
+    <>
+      {/* departments */}
+      <Grid container spacing={{ xs: 2, md: 3 }}>
+        <Grid item xs={6} md={3}>
+          <Box component={Link} to='/'>
+            <Box
+              component='img'
+              src='../images/department_women.jpg'
+              sx={{ width: '100%', objectFit: 'cover' }}
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <Box component={Link} to='/'>
+            <Box
+              component='img'
+              src='../images/department_men.jpg'
+              sx={{ width: '100%', objectFit: 'cover' }}
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <Box component={Link} to='/'>
+            <Box
+              component='img'
+              src='../images/department_kids.jpg'
+              sx={{ width: '100%', objectFit: 'cover' }}
+            />
+          </Box>
+        </Grid>
+        <Grid item xs={6} md={3}>
+          <Box component={Link} to='/'>
+            <Box
+              component='img'
+              src='../images/department_beauty.jpg'
+              sx={{ width: '100%', objectFit: 'cover' }}
+            />
+          </Box>
+        </Grid>
+      </Grid>
+
+      {/* feature products */}
+      <Grid container spacing={3}>
+        {dummy_products.map((product, index) => {
+          return (
+            <Grid key={index} item xs={12} sm={6} md={4} lg={2}>
+              <Card
+                elevation={0}
+                component={Link}
+                to={`products/${product.id}`}
+                sx={{ textDecoration: 'none' }}
+              >
+                <CardMedia
+                  sx={{ height: 300 }}
+                  image='../images/mens_shirt.jpg'
+                  title={product.title}
+                />
+                <CardContent sx={{ padding: '5px' }}>
+                  <Typography
+                    component='h3'
+                    variant='h6'
+                    noWrap
+                    sx={{ fontWeight: 'bold', fontSize: '14px' }}
+                  >
+                    {product.title} - {product.productDetails.color}
                   </Typography>
-                  <IconButton>
-                    <FavoriteBorderIcon />
-                  </IconButton>
-                </Box>
-              </CardContent>
-            </Card>
-          </Grid>
-        );
-      })}
-    </Grid>
+                  <Typography
+                    component='p'
+                    variant='body2'
+                    sx={{ color: '#888', fontSize: '12px' }}
+                  >
+                    By {product.productDetails.brand}
+                  </Typography>
+                  <Box
+                    component='div'
+                    sx={{
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'space-between',
+                    }}
+                  >
+                    <Typography component='p' sx={{ fontSize: '16px' }}>
+                      ${product.price.toFixed(2)}
+                    </Typography>
+                    <IconButton>
+                      <FavoriteBorderIcon />
+                    </IconButton>
+                  </Box>
+                </CardContent>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </>
   );
 }
 
