539fab69008173b3e9f7a3a8aa1e17e2e29df077
Jon-Felipe <rodriguesjf7@gmail.com>
2024-06-17 23:02:23 +0100

    home page refactoring

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,6 @@
 // components
 import DepartmentLinkCard from '../components/home/DepartmentLinkCard';
 
-// mui components
-import Grid from '@mui/material/Grid';
-
 // extras
 import { dummy_products } from '../utils/constants';
 import ProductCard from '../components/ProductCard';
@@ -15,15 +12,15 @@ function Home() {
       <DepartmentLinkCard />
 
       {/* feature products */}
-      <Grid container spacing={3} marginTop={5}>
+      <section className='grid grid-cols-2 lg:grid-cols-6 gap-4 mt-20'>
         {dummy_products.map((product, index) => {
           return (
-            <Grid key={index} item xs={12} sm={6} md={4} lg={2}>
+            <article key={index}>
               <ProductCard product={product} />
-            </Grid>
+            </article>
           );
         })}
-      </Grid>
+      </section>
     </>
   );
 }
