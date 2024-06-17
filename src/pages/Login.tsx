1ad020e0ee8a399e1d1b0ed731a0d0d13030d47c
Jon-Felipe <rodriguesjf7@gmail.com>
2024-06-17 14:46:31 +0100

    login fields

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,50 @@
+// mui components
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+
 function Login() {
-  return <div>Login</div>;
+  return (
+    <Box maxWidth='sm' margin='80px auto 0'>
+      <Box component='form'>
+        <Typography component='h3' variant='h6' fontWeight='600'>
+          Login
+        </Typography>
+        <TextField
+          label='Email'
+          type='email'
+          variant='outlined'
+          fullWidth
+          margin='normal'
+        />
+        <TextField
+          label='Password'
+          type='password'
+          variant='outlined'
+          fullWidth
+          margin='normal'
+        />
+        <Box marginY={1}>
+          <Button type='submit' variant='contained' fullWidth size='large'>
+            Submit
+          </Button>
+        </Box>
+        <Typography>
+          Create an account?{' '}
+          <span>
+            <Button>Click here</Button>
+          </span>
+        </Typography>
+        <FormControlLabel
+          control={<Checkbox />}
+          label='By continuing, I agree to the terms of use & privacy policy.'
+        />
+      </Box>
+    </Box>
+  );
 }
 
 export default Login;
