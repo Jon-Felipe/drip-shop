9fdaa7361f9b6290b20d343320227631623ef5de
Jon-Felipe <rodriguesjf7@gmail.com>
2024-06-18 21:18:53 +0100

    favourite icon bug fix

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,19 +10,19 @@ type Props = {
 
 function ProductCard({ product }: Props) {
   return (
-    <Link to={`/products/${product.id}`}>
-      <article>
+    <article className='relative'>
+      <Link to={`/products/${product.id}`}>
         <img src='../images/mens_shirt.jpg' alt={product.title} />
         <div className='relative mt-2'>
           <h4 className='truncate text-sm font-semibold'>{product.title}</h4>
           <p className='text-neutral-400 text-xs'>By {product.brand}</p>
           <p className='text-lg'>R{product.price}</p>
-          <div className='absolute right-0 bottom-0 cursor-pointer'>
-            <MdFavoriteBorder className='w-5 h-5' />
-          </div>
         </div>
-      </article>
-    </Link>
+      </Link>
+      <div className='absolute right-0 bottom-0 cursor-pointer'>
+        <MdFavoriteBorder className='w-5 h-5' />
+      </div>
+    </article>
   );
 }
 
