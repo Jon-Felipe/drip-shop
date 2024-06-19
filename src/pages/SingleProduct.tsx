c975244146c4124070931f5bc9315073e2421f09
Jon-Felipe <rodriguesjf7@gmail.com>
2024-06-19 12:03:37 +0100

    refactored single product screen

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -2,28 +2,24 @@ import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useAppDispatch } from '../hooks/hooks';
 import { addToCart } from '../slices/cartSlice';
-
-// mui components
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import RemoveIcon from '@mui/icons-material/Remove';
-import AddIcon from '@mui/icons-material/Add';
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import Rating from '@mui/material/Rating';
+import {
+  MdOutlineRemove,
+  MdAdd,
+  MdOutlineShoppingCart,
+  MdFavorite,
+} from 'react-icons/md';
 
 // extras
 import { dummy_products } from '../utils/constants';
 import { ICart } from '../utils/types';
 
-function SingleProduct() {
-  const [size, setSize] = useState<{ size: string; countInStock: number }>({
-    size: '',
-    countInStock: 0,
-  });
+type Props = {};
+
+function SingleProduct({}: Props) {
+  const [inventory, setInventory] = useState<{
+    size: string;
+    countInStock: number;
+  }>({ size: '', countInStock: 0 });
   const [quantity, setQuantity] = useState<number>(1);
 
   const dispatch = useAppDispatch();
@@ -36,8 +32,8 @@ function SingleProduct() {
 
   function handleOnToggleQuantity(value: string) {
     if (value === 'increase') {
-      if (quantity === size.countInStock) {
-        setQuantity(size.countInStock);
+      if (quantity === inventory.countInStock) {
+        setQuantity(inventory.countInStock);
       } else {
         setQuantity(quantity + 1);
       }
@@ -61,7 +57,7 @@ function SingleProduct() {
         price: foundProduct?.price || 0,
         colour: foundProduct?.colour || '',
       },
-      size: size.size,
+      size: inventory.size,
       quantity,
     };
     dispatch(addToCart(cartObj));
@@ -69,221 +65,115 @@ function SingleProduct() {
   }
 
   return (
-    <Grid container spacing={4}>
+    <div className='grid lg:grid-cols-2 gap-x-8'>
       {/* image gallery */}
-      <Grid item xs={12} sm={5} component='section'>
+      <section>
         <img
           src='../images/mens_shirt.jpg'
           alt={foundProduct?.title}
-          style={{ width: '100%', objectFit: 'cover' }}
+          className='w-full object-cover'
         />
-      </Grid>
+      </section>
       {/* product */}
-      <Grid item xs={12} sm={7} component='section'>
-        {/* product title */}
-        <Typography component='h3' variant='h5' gutterBottom>
-          {foundProduct?.title}
-        </Typography>
-        {/* product brand */}
-        <Typography component='p' sx={{ color: '#888' }}>
+      <section className='mt-4'>
+        <h3 className='text-xl font-semibold'>{foundProduct?.title}</h3>
+        <p className='text-neutral-400 font-light text-lg'>
           {foundProduct?.brand}
-        </Typography>
-        {/* product rating */}
-        <Box
-          sx={{
-            display: 'flex',
-            alignItems: 'start',
-            columnGap: '5px',
-            marginY: '10px',
-          }}
-        >
-          <Rating
-            value={foundProduct?.averageRating}
-            precision={0.5}
-            readOnly
-            size='small'
-          />
-          <Typography component='p' variant='caption'>
-            (122)
-          </Typography>
-        </Box>
-        {/* product price */}
-        <Typography component='p' variant='h4'>
-          ${foundProduct?.price.toFixed(2)}
-        </Typography>
-        {/* product size */}
-        <Box>
-          <Typography
-            component='p'
-            variant='h6'
-            marginTop='20px'
-            marginBottom='10px'
-          >
-            Select a size
-          </Typography>
-          <Box sx={{ display: 'flex', columnGap: '20px' }}>
+        </p>
+        <div className='mt-6'>
+          <p className='text-3xl'>R{foundProduct?.price}</p>
+        </div>
+        {/* size selector */}
+        <div className='mt-6'>
+          <h6 className='font-semibold uppercase text-sm'>Select a size</h6>
+          <div className='flex items-center gap-x-6 mt-4'>
             {foundProduct?.inventory.sizes.map((s, index) => {
               return (
-                <Box
+                <div
                   key={index}
-                  component='div'
-                  onClick={() => setSize(s)}
-                  sx={{
-                    border: `2px solid ${
-                      size.size === s.size ? '#f58723' : '#d9d9d9'
-                    }`,
-                    borderRadius: '28px',
-                    color: `${size.size === s.size ? '#f58723' : '#000'}`,
-                    boxSizing: 'border-box',
-                    display: 'inline-flex',
-                    flexShrink: 0,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    height: '56px',
-                    minWidth: '56px',
-                    padding: '0 12px',
-                    cursor: 'pointer',
+                  className={`border ${
+                    inventory.size === s.size ? 'border-2 border-black' : ''
+                  }  rounded-full h-14 min-w-14 box-border inline-flex items-center justify-center shrink-0 px-3 cursor-pointer`}
+                  onClick={() => {
+                    setInventory(s);
+                    setQuantity(1);
                   }}
                 >
-                  <Typography component='p' sx={{ textTransform: 'uppercase' }}>
-                    {s.size}
-                  </Typography>
-                </Box>
+                  <p className='uppercase font-semibold'>{s.size}</p>
+                </div>
               );
             })}
-          </Box>
-        </Box>
-        {/* product quantity */}
-        {size.size && (
-          <Box
-            sx={{ display: 'flex', alignItems: 'center', marginTop: '20px' }}
-          >
-            <Typography component='p' variant='h6'>
-              Quantity
-            </Typography>
-            <Box
-              sx={{
-                display: 'flex',
-                alignItems: 'center',
-                columnGap: '40px',
-                marginLeft: '100px',
-              }}
-            >
-              <IconButton onClick={() => handleOnToggleQuantity('decrease')}>
-                <RemoveIcon />
-              </IconButton>
-              <Typography component='p'>{quantity}</Typography>
-              <IconButton onClick={() => handleOnToggleQuantity('increase')}>
-                <AddIcon />
-              </IconButton>
-            </Box>
-          </Box>
+          </div>
+        </div>
+        {/* quantity selector */}
+        {inventory.size && (
+          <div className='mt-6 flex items-center gap-x-14'>
+            <h6 className='font-semibold uppercase text-sm'>Quantity</h6>
+            <div className='flex items-center justify-center gap-x-14'>
+              <button onClick={() => handleOnToggleQuantity('decrease')}>
+                <MdOutlineRemove className='w-5 h-5' />
+              </button>
+              <p>{quantity}</p>
+              <button onClick={() => handleOnToggleQuantity('increase')}>
+                <MdAdd className='w-5 h-5' />
+              </button>
+            </div>
+          </div>
         )}
-        {/* action buttons */}
-        <Box
-          component='div'
-          sx={{
-            width: '100%',
-            maxWidth: '500px',
-            display: 'flex',
-            columnGap: '20px',
-            marginTop: '20px',
-          }}
-        >
-          <Button
-            variant='contained'
-            size='large'
-            color='error'
-            startIcon={<ShoppingCartOutlinedIcon />}
-            sx={{ width: '100%', fontSize: { xs: '12px', sm: '14px' } }}
+        {/* product action buttons */}
+        <div className='mt-6 flex flex-col lg:flex-row lg:items-center gap-y-4 lg:gap-x-4'>
+          <button
             onClick={handleAddToCart}
+            className='w-full bg-red-500 text-white font-bold py-2 uppercase flex items-center justify-center gap-x-2'
           >
+            <MdOutlineShoppingCart />
             Add to cart
-          </Button>
-          <Button
-            variant='outlined'
-            color='inherit'
-            size='large'
-            startIcon={<FavoriteIcon />}
-            sx={{ width: '100%', fontSize: { xs: '12px', sm: '14px' } }}
-          >
+          </button>
+          <button className='w-full border py-2 font-bold uppercase flex items-center justify-center gap-x-2'>
+            <MdFavorite />
             Add to wishlist
-          </Button>
-        </Box>
+          </button>
+        </div>
         {/* product details */}
-        <Box component='div' marginTop='20px'>
-          <Typography component='h6' sx={{ color: '#888', fontSize: '16px' }}>
-            Product Details
-          </Typography>
+        <div className='mt-6'>
+          <h6 className='text-neutral-400 font-medium'>Product Details</h6>
           {foundProduct?.description && (
-            <Typography
-              component='p'
-              sx={{ color: '#888', marginTop: '5px', fontSize: '14px' }}
-            >
+            <p className='text-neutral-400 text-sm'>
               {foundProduct.description}
-            </Typography>
+            </p>
           )}
           {foundProduct?.extraInfo && foundProduct?.extraInfo?.length > 0 && (
-            <ul>
-              {foundProduct.extraInfo.map((item, index) => {
-                return (
-                  <li key={index} style={{ color: '#888' }}>
-                    <Typography
-                      component='p'
-                      sx={{
-                        margin: '5px 0',
-                        fontSize: '14px',
-                      }}
-                    >
-                      {item}
-                    </Typography>
-                  </li>
-                );
-              })}
+            <ul className='mt-4 ml-8'>
+              {foundProduct.extraInfo.map((item, index) => (
+                <li className='list-disc text-neutral-400' key={index}>
+                  {item}
+                </li>
+              ))}
             </ul>
           )}
-          <Grid container spacing={2}>
-            <Grid item xs={6}>
-              <Typography sx={{ fontSize: '16px', fontWeight: 'bold' }}>
-                Colour
-              </Typography>
-              <Typography
-                component='p'
-                sx={{
-                  color: '#888',
-                  fontSize: '14px',
-                  textTransform: 'capitalize',
-                }}
-              >
+          <div className='mt-8 grid grid-cols-4 md:grid-cols-2 gap-4'>
+            <div>
+              <h6 className='font-semibold'>Colour</h6>
+              <p className='capitalize text-neutral-400 text-sm'>
                 {foundProduct?.colour}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography sx={{ fontSize: '16px', fontWeight: 'bold' }}>
-                Brand
-              </Typography>
-              <Typography
-                component='p'
-                sx={{ color: '#888', fontSize: '14px' }}
-              >
+              </p>
+            </div>
+            <div>
+              <h6 className='font-semibold'>Brand</h6>
+              <p className='capitalize text-neutral-400 text-sm'>
                 {foundProduct?.brand}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              <Typography sx={{ fontSize: '16px', fontWeight: 'bold' }}>
-                Material
-              </Typography>
-              <Typography
-                component='p'
-                sx={{ color: '#888', fontSize: '14px' }}
-              >
+              </p>
+            </div>
+            <div>
+              <h6 className='font-semibold'>Material</h6>
+              <p className='capitalize text-neutral-400 text-sm'>
                 {foundProduct?.material}
-              </Typography>
-            </Grid>
-          </Grid>
-        </Box>
-      </Grid>
-    </Grid>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 }
 
