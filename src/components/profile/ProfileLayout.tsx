bf4860c17a9cd2f50ded1b3a43b62107610cc6ba
Jon-Felipe <rodriguesjf7@gmail.com>
2024-06-21 23:35:35 +0100

    profile layout design

--- src/components/profile/ProfileLayout.tsx
+++ src/components/profile/ProfileLayout.tsx
@@ -1,11 +1,52 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
+import {
+  MdOutlinePerson,
+  MdOutlineLock,
+  MdOutlineInventory2,
+  MdLogout,
+} from 'react-icons/md';
 
-type Props = {};
-
-function ProfileLayout({}: Props) {
+function ProfileLayout() {
   return (
     <>
-      <Outlet />
+      <section>
+        <h3 className='text-xl lg:text-3xl font-semibold'>Account Settings</h3>
+        <p className='mt-2'>Manage your profile and account settings</p>
+      </section>
+      <div className='grid lg:grid-cols-[300px_1fr] lg:gap-x-4 mt-8'>
+        <section>
+          <div className='flex flex-col gap-y-4'>
+            <Link
+              to='/profile'
+              className='flex items-center gap-x-2 bg-blue-500 text-white rounded shadow py-2 px-4'
+            >
+              <MdOutlinePerson className='w-5 h-5' />
+              <span>Account</span>
+            </Link>
+            <Link
+              to='/my-password'
+              className='flex items-center gap-x-2 py-2 px-4'
+            >
+              <MdOutlineLock className='w-5 h-5' />
+              <span>Password</span>
+            </Link>
+            <Link
+              to='/my-orders'
+              className='flex items-center gap-x-2 py-2 px-4'
+            >
+              <MdOutlineInventory2 className='w-5 h-5' />
+              <span>My Orders</span>
+            </Link>
+            <button className='flex items-center gap-x-2 py-2 px-4'>
+              <MdLogout className='w-5 h-5' />
+              <span>Logout</span>
+            </button>
+          </div>
+        </section>
+        <section>
+          <Outlet />
+        </section>
+      </div>
     </>
   );
 }
