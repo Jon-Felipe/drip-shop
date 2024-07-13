6ae39d56045d881584f20df8bf9aad6575250974
Jon-Felipe <rodriguesjf7@gmail.com>
2024-07-13 15:13:34 +0200

    login user functionality

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,47 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAppDispatch } from '../hooks/hooks';
+import { useLoginMutation } from '../slices/apiSlice';
+import { setUser } from '../slices/userSlice';
+
 // components
 import Input from '../components/Input';
 
+// extras
+import { IUser } from '../utils/types';
+
 function Login() {
+  const [userInfo, setUserInfo] = useState<Pick<IUser, 'email' | 'password'>>({
+    email: '',
+    password: '',
+  });
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const [login, { isLoading }] = useLoginMutation();
+
+  function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserInfo((prevState) => {
+      return { ...prevState, [name]: value };
+    });
+  }
+
+  async function handleOnSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    try {
+      const user = await login({
+        email: userInfo.email,
+        password: userInfo.password,
+      }).unwrap();
+      dispatch(setUser(user));
+      navigate('/');
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <div className='p-4 w-full max-w-xl max-h-full mx-auto'>
       <div className='bg-white rounded-lg border'>
@@ -11,14 +51,14 @@ function Login() {
           </h3>
         </div>
         <div className='p-4 md:p-5'>
-          <form className='space-y-4'>
+          <form className='space-y-4' onSubmit={handleOnSubmit}>
             <div>
               <Input
                 label='Email'
                 type='email'
                 name='email'
-                value={''}
-                onChange={() => console.log('first')}
+                value={userInfo.email}
+                onChange={handleOnChange}
                 placeholder='name@example.com'
                 required={true}
               />
@@ -28,8 +68,8 @@ function Login() {
                 label='Your password'
                 type='password'
                 name='password'
-                value={''}
-                onChange={() => console.log('first')}
+                value={userInfo.password}
+                onChange={handleOnChange}
                 placeholder='••••••••'
                 required={true}
               />
@@ -60,7 +100,7 @@ function Login() {
               type='submit'
               className='w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center'
             >
-              Login to your account
+              {isLoading ? 'Loading...' : 'Login to your account'}
             </button>
             <div className='text-sm font-medium text-gray-500'>
               Not registered?{' '}
