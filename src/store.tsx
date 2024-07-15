216d5449421986ad58ac312b3daa5644ca455cb9
Jon-Felipe <rodriguesjf7@gmail.com>
2024-07-15 10:47:24 +0200

    typo fix

--- src/store.tsx
+++ src/store.tsx
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './slices/cartSlice';
-import useReducer from './slices/userSlice';
+import userReducer from './slices/userSlice';
 import { dripShopApi } from './slices/apiSlice';
 
 export const store = configureStore({
   reducer: {
     [dripShopApi.reducerPath]: dripShopApi.reducer,
-    user: useReducer,
+    user: userReducer,
     cart: cartReducer,
   },
   middleware: (getDefaultMiddleware) =>
