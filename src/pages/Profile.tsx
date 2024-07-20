3ca1114643bc53a2a6972575ea53cb6371f31eaa
Jon-Felipe <rodriguesjf7@gmail.com>
2024-07-20 11:57:31 +0200

    loading text

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -161,7 +161,7 @@ function Profile() {
           disabled={isLoading}
           className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center'
         >
-          Update
+          {isLoading ? 'Submitting...' : 'Update'}
         </button>
       </form>
     </>
