721d7d59b2d90f8fe8380c1582fa1dc89a722776
Jon-Felipe <rodriguesjf7@gmail.com>
2024-08-10 13:45:49 +0200

    removed feature products

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,7 @@
 import DepartmentLinkCard from '../components/home/DepartmentLinkCard';
 
 // extras
-import { dummy_brands, dummy_products } from '../utils/constants';
-import ProductCard from '../components/ProductCard';
+import { dummy_brands } from '../utils/constants';
 
 function Home() {
   return (
@@ -17,17 +16,6 @@ function Home() {
           <p>{brand.title}</p>
         </div>
       ))}
-
-      {/* feature products */}
-      <section className='grid grid-cols-2 lg:grid-cols-6 gap-4 mt-20'>
-        {dummy_products.map((product, index) => {
-          return (
-            <article key={index}>
-              <ProductCard product={product} />
-            </article>
-          );
-        })}
-      </section>
     </>
   );
 }
