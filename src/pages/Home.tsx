81e1357ba5ae1f3e54ed42269023f9b70733baa4
Jon-Felipe <rodriguesjf7@gmail.com>
2024-08-10 13:45:22 +0200

    displaying dummy brand

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@
 import DepartmentLinkCard from '../components/home/DepartmentLinkCard';
 
 // extras
-import { dummy_products } from '../utils/constants';
+import { dummy_brands, dummy_products } from '../utils/constants';
 import ProductCard from '../components/ProductCard';
 
 function Home() {
@@ -10,6 +10,13 @@ function Home() {
     <>
       {/* departments */}
       <DepartmentLinkCard />
+
+      {/* brands */}
+      {dummy_brands.map((brand) => (
+        <div key={brand.id}>
+          <p>{brand.title}</p>
+        </div>
+      ))}
 
       {/* feature products */}
       <section className='grid grid-cols-2 lg:grid-cols-6 gap-4 mt-20'>
