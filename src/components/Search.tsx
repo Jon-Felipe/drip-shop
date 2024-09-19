dccc7a84d4468316ead5512647e473af5b73cbf3
Jon-Felipe <rodriguesjf7@gmail.com>
2024-09-19 22:11:00 +0200

    search input handle search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,8 +1,23 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { MdOutlineSearch } from 'react-icons/md';
+import { toast } from 'react-toastify';
 
 function Search() {
+  const [search, setSearch] = useState<string>('');
+
+  const navigate = useNavigate();
+
+  function handleOnSearch() {
+    if (!search) {
+      toast.error('Please provide a search value');
+      return;
+    }
+    navigate(`/products?search=${search}`);
+  }
+
   return (
-    <form className='w-full'>
+    <div className='w-full'>
       <label
         htmlFor='search'
         className='mb-2 text-sm font-medium text-gray-900 sr-only'
@@ -16,18 +31,21 @@ function Search() {
         <input
           type='search'
           id='search'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
           className='block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500'
           placeholder='Search'
           required
         />
         <button
           type='submit'
+          onClick={handleOnSearch}
           className='text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2'
         >
           Search
         </button>
       </div>
-    </form>
+    </div>
   );
 }
 
