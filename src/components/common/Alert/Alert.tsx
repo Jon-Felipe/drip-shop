7f137fe4c45780fed45430d58583ed155b201046
Jon-Felipe <rodriguesjf7@gmail.com>
2024-11-08 10:35:25 +0200

    alert icon prop

--- src/components/common/Alert/Alert.tsx
+++ src/components/common/Alert/Alert.tsx
@@ -5,16 +5,23 @@ const alertVariants = {
   warning: 'text-yellow-800 bg-yellow-50',
 };
 
-type AlertProps = {
+export type AlertProps = {
   variant?: 'info' | 'danger' | 'success' | 'warning';
   message: string;
+  icon?: React.ReactNode;
 };
 
-function Alert({ variant = 'info', message }: AlertProps) {
+function Alert({ variant = 'info', message, icon }: AlertProps) {
   const variantClass = alertVariants[variant];
   return (
-    <div className={`p-4 mb-4 text-sm rounded-lg ${variantClass}`} role='alert'>
-      <span className='font-medium capitalize'>{variant}!</span> {message}
+    <div
+      className={`w-full flex items-center p-4 mb-4 text-sm rounded-lg ${variantClass}`}
+      role='alert'
+    >
+      {icon && icon}
+      <div>
+        <span className='font-medium capitalize'>{variant}!</span> {message}
+      </div>
     </div>
   );
 }
