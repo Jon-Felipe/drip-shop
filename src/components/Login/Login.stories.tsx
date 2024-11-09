ad8fc1106d6d97284cea534195ebb346d0f2f7d9
Jon-Felipe <rodriguesjf7@gmail.com>
2024-11-09 15:58:03 +0200

    login component and story init

---
+++ src/components/Login/Login.stories.tsx
@@ -0,0 +1,31 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import Login from './Login';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
+const meta: Meta<typeof Login> = {
+  title: 'Components/Login',
+  component: Login,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'padded',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  // More on argTypes: https://storybook.js.org/docs/api/argtypes
+  argTypes: {},
+  decorators: [
+    (Story) => (
+      <MemoryRouter initialEntries={['/login']}>
+        <Story />
+      </MemoryRouter>
+    ),
+  ],
+};
+
+export default meta;
+type Story = StoryObj<typeof Login>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const DefaultLogin: Story = {};
