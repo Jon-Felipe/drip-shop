9c7ad0b7a2229834f43af2e372c081c218067161
Jon-Felipe <rodriguesjf7@gmail.com>
2024-12-27 16:33:10 +0200

    login form social and action buttons

--- src/components/Login/Login.styles.tsx
+++ src/components/Login/Login.styles.tsx
@@ -6,16 +6,17 @@ export const FormWrapper = styled.form`
 
   .title {
     font-size: 30px;
-    font-weight: 900;
-    margin-bottom: 0px;
+    font-weight: 600;
+    margin: 0 0 5px 0;
   }
 
   .sub-text {
     font-size: 16px;
     font-weight: 300;
+    margin: 0px;
 
     span {
-      font-weight: 700;
+      font-weight: 600;
       color: rgb(26 86 219);
       cursor: pointer;
     }
@@ -23,9 +24,64 @@ export const FormWrapper = styled.form`
 
   .form-inputs {
     display: flex;
-    flex-direction: column;
-    row-gap: 16px;
-    width: 100%;
-    margin-bottom: 16px;
+    align-items: center;
+    column-gap: 14px;
+    margin: 24px 0 0;
+  }
+
+  .divider {
+    display: flex;
+    align-items: center;
+    column-gap: 14px;
+
+    hr {
+      width: 100%;
+      height: 2px;
+      background-color: rgb(229 231 235);
+      border: none;
+    }
+  }
+
+  .action-helpers {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    align-items: center;
+    justify-content: space-between;
+    margin-bottom: 10px;
+
+    .remember {
+      display: flex;
+      align-items: flex-end;
+      column-gap: 4px;
+    }
+  }
+`;
+
+export const SocialButtonWrapper = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  column-gap: 6px;
+  width: 100%;
+  background: none;
+  border: 1px solid rgb(229 231 235);
+  border-radius: 8px;
+  font-family: 'Funnel Display', sans-serif;
+  font-size: 16px;
+  padding: 8px 0;
+  margin-bottom: 20px;
+  cursor: pointer;
+
+  span {
+    margin-top: 3px;
+  }
+
+  svg {
+    width: 20px;
+    height: 20px;
+  }
+
+  &:hover {
+    background-color: #f5f5f5;
   }
 `;
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,16 +1,19 @@
+import { FcGoogle } from 'react-icons/fc';
+import { FaApple } from 'react-icons/fa';
+
 // components
 import Input from '../common/Input/Input';
 import Button from '../common/Button/Button';
 
 // extras
-import { FormWrapper } from './Login.styles';
+import { FormWrapper, SocialButtonWrapper } from './Login.styles';
 
 type Props = {};
 
 function Login({}: Props) {
   return (
     <FormWrapper>
-      <h3 className='title'>Welcome Back</h3>
+      <h3 className='title'>Welcome back</h3>
       <p className='sub-text'>
         Browse all the latest fashion and trends. Don't have an account?{' '}
         <span>Sign up</span>
@@ -34,6 +37,34 @@ function Login({}: Props) {
           placeholder='••••••••'
           required
         />
+      </div>
+      <div className='divider'>
+        <hr />
+        <p>or</p>
+        <hr />
+      </div>
+      <div>
+        <SocialButtonWrapper>
+          <span>
+            <FcGoogle />
+          </span>
+          Sign in with Google{' '}
+        </SocialButtonWrapper>
+        <SocialButtonWrapper>
+          <span>
+            <FaApple />
+          </span>
+          Sign in with Apple{' '}
+        </SocialButtonWrapper>
+      </div>
+      <div className='action-helpers'>
+        <div className='remember'>
+          <input type='checkbox' id='remember' name='remember' />
+          <label htmlFor='remember'>Remember me</label>
+        </div>
+        <Button type='button' variant='text'>
+          Forgot Password?
+        </Button>
       </div>
       <Button type='submit'>Login</Button>
     </FormWrapper>
