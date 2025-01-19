00bd856fd0d5d5234cb8683ad6d3878041e1fd39
Jon-Felipe <rodriguesjf7@gmail.com>
2025-01-19 12:15:31 +0200

    routes location refactor

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ import Products from './pages/Products.tsx';
 import SingleProduct from './pages/SingleProduct.tsx';
 import Cart from './pages/Cart.tsx';
 import Department from './pages/Department.tsx';
-import Login from './pages/Login.tsx';
+import Login from './pages/Login/Login.tsx';
 import Register from './pages/Register.tsx';
 import Profile from './pages/Profile.tsx';
 import ProfileLayout from './components/profile/ProfileLayout.tsx';
@@ -27,28 +27,32 @@ import Checkout from './pages/Checkout.tsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/' element={<App />}>
-      <Route path='' element={<Home />} />
-      <Route path='department/:department' element={<Department />} />
-      <Route path='products' element={<Products />} />
-      <Route path='products/:id' element={<SingleProduct />} />
-      <Route element={<ProtectedRouteLayout />}>
-        <Route
-          path='profile'
-          element={<ProfileLayout />}
-          children={
-            <Route>
-              <Route path='' element={<Profile />} />
-              <Route path='update-password' element={<UpdatePassword />} />
-            </Route>
-          }
-        />
+    <>
+      <Route path='/' element={<App />}>
+        <Route path='' element={<Home />} />
+        <Route path='department/:department' element={<Department />} />
+        <Route path='products' element={<Products />} />
+        <Route path='products/:id' element={<SingleProduct />} />
+        <Route path='cart' element={<Cart />} />
         <Route path='checkout' element={<Checkout />} />
+        <Route element={<ProtectedRouteLayout />}>
+          <Route
+            path='profile'
+            element={<ProfileLayout />}
+            children={
+              <Route>
+                <Route path='' element={<Profile />} />
+                <Route path='update-password' element={<UpdatePassword />} />
+              </Route>
+            }
+          />
+        </Route>
       </Route>
-      <Route path='cart' element={<Cart />} />
-      <Route path='login' element={<Login />} />
-      <Route path='register' element={<Register />} />
-    </Route>
+      <Route>
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
+      </Route>
+    </>
   )
 );
 
