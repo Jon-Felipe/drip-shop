ac456efaacce0fe4e409ab32312c2af76b8b9c22
Jon-Felipe <rodriguesjf7@gmail.com>
2025-01-19 12:52:05 +0200

    replaced Login component with Login page

---
+++ src/pages/Login/Login.styles.tsx
@@ -0,0 +1,112 @@
+import styled from 'styled-components';
+
+export const FormWrapper = styled.div`
+  display: block;
+  margin-top: 150px;
+  padding: 0 20px;
+
+  .form-section {
+    width: 100%;
+    max-width: 700px;
+    margin: 0 auto;
+    .title {
+      font-size: 26px;
+      font-weight: 600;
+      margin: 0 0 5px 0;
+    }
+
+    .sub-text {
+      font-size: 16px;
+      font-weight: 300;
+      margin-bottom: 16px;
+    }
+
+    .form-inputs {
+      display: flex;
+      align-items: center;
+      column-gap: 14px;
+      width: 100%;
+    }
+
+    .divider {
+      display: flex;
+      align-items: center;
+      column-gap: 14px;
+      margin: 16px 0;
+
+      hr {
+        width: 100%;
+        height: 2px;
+        background-color: rgb(229 231 235);
+        border: none;
+      }
+    }
+
+    .action-helpers {
+      display: grid;
+      grid-template-columns: 1fr auto;
+      align-items: center;
+      justify-content: space-between;
+      margin-bottom: 10px;
+
+      .remember {
+        display: flex;
+        align-items: center;
+        column-gap: 4px;
+      }
+    }
+  }
+
+  .image-section {
+    img {
+      display: none;
+    }
+  }
+
+  @media only screen and (min-width: 1024px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    column-gap: 40px;
+    place-items: center;
+
+    .image-section {
+      width: 100%;
+      height: 100%;
+      img {
+        display: inline-block;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
+  }
+`;
+
+export const SocialButtonWrapper = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  column-gap: 6px;
+  width: 100%;
+  background: none;
+  border: 1px solid rgb(229 231 235);
+  border-radius: 8px;
+  font-family: 'Funnel Display', sans-serif;
+  font-size: 16px;
+  padding: 8px 0;
+  margin-bottom: 20px;
+  cursor: pointer;
+
+  span {
+    margin-top: 3px;
+  }
+
+  svg {
+    width: 20px;
+    height: 20px;
+  }
+
+  &:hover {
+    background-color: #f5f5f5;
+  }
+`;
---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,104 @@
+import React, { useState } from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { FaApple } from 'react-icons/fa';
+
+// components
+import Link from '../../components/common/Link/Link';
+import Input from '../../components/common/Input/Input';
+import Button from '../../components/common/Button/Button';
+
+// extras
+import { FormWrapper, SocialButtonWrapper } from './Login.styles';
+import LoginImage from '../../../public/images/login_img.svg';
+
+interface ILogin {
+  email: string;
+  password: string;
+}
+
+function Login() {
+  const [loginDetails, setLoginDetails] = useState<ILogin>({
+    email: '',
+    password: '',
+  });
+
+  function handleOnChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const name = e.target.name;
+    const value = e.target.value;
+    setLoginDetails((prevState) => ({ ...prevState, [name]: value }));
+  }
+
+  function handleOnSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    alert('login');
+  }
+
+  return (
+    <FormWrapper>
+      <section className='form-section'>
+        <form onSubmit={handleOnSubmit}>
+          <h3 className='title'>Welcome back</h3>
+          <p className='sub-text'>
+            Browse all the latest fashion and trends. Don't have an account?{' '}
+            <Link path='/register' text='Sign up' underlined='always' />
+          </p>
+          <div className='form-inputs'>
+            <Input
+              label='Email'
+              type='text'
+              name='email'
+              value={loginDetails.email}
+              onChange={handleOnChange}
+              placeholder='name@email.com'
+              required
+            />
+            <Input
+              label='Password'
+              type='password'
+              name='password'
+              value={loginDetails.password}
+              onChange={handleOnChange}
+              placeholder='••••••••'
+              required
+            />
+          </div>
+          <div className='divider'>
+            <hr />
+            <p>or</p>
+            <hr />
+          </div>
+          <div>
+            <SocialButtonWrapper>
+              <span>
+                <FcGoogle />
+              </span>
+              Sign in with Google{' '}
+            </SocialButtonWrapper>
+            <SocialButtonWrapper>
+              <span>
+                <FaApple />
+              </span>
+              Sign in with Apple{' '}
+            </SocialButtonWrapper>
+          </div>
+          <div className='action-helpers'>
+            <div className='remember'>
+              <input type='checkbox' id='remember' name='remember' />
+              <label htmlFor='remember'>Remember me</label>
+            </div>
+            <Button type='button' variant='text'>
+              Forgot Password?
+            </Button>
+          </div>
+          <Button type='submit'>Login</Button>
+        </form>
+      </section>
+
+      <section className='image-section'>
+        <img src={LoginImage} alt='login' />
+      </section>
+    </FormWrapper>
+  );
+}
+
+export default Login;
