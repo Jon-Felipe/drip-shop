4ca9342cdae9675042f0922e2480875c64835113
Jon-Felipe <rodriguesjf7@gmail.com>
2025-02-02 19:50:45 +0200

    deparment styling updates

--- src/pages/Department/Department.styles.tsx
+++ src/pages/Department/Department.styles.tsx
@@ -7,6 +7,7 @@ export const DepartmentWrapper = styled.div`
   max-width: 1140px;
   margin: 40px auto 0;
   display: grid;
+  column-gap: 20px;
 
   @media only screen and (min-width: 1024px) {
     grid-template-columns: 300px 1fr;
@@ -22,6 +23,7 @@ export const FilterWrapper = styled.section`
 
   @media only screen and (min-width: 1024px) {
     display: block;
+    padding: 20px;
   }
 `;
 
