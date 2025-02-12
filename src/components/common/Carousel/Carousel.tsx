2e373a2c9ad47b187381e8822f9f6ba8dedfe7d2
Jon-Felipe <rodriguesjf7@gmail.com>
2025-02-12 12:08:22 +0200

    carousel component, styles and stories init

---
+++ src/components/common/Carousel/Carousel.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// extras
+import { CarouselWrapper } from './Carousel.styles';
+
+type Props = {};
+
+function Carousel({}: Props) {
+  return <CarouselWrapper>Carousel</CarouselWrapper>;
+}
+
+export default Carousel;
