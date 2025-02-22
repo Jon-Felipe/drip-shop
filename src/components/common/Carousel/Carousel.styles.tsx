9c397b555685563828f3d7d1088cc58a213108f2
Jon-Felipe <rodriguesjf7@gmail.com>
2025-02-22 19:52:46 +0200

    carousel component and styling displaying brands

--- src/components/common/Carousel/Carousel.styles.tsx
+++ src/components/common/Carousel/Carousel.styles.tsx
@@ -1,15 +1,9 @@
 import styled from 'styled-components';
 
 export const CarouselWrapper = styled.div`
-  min-height: 0px;
-  min-width: 0px;
-  padding: 20px;
-  h3 {
-    font-size: 38px;
-    margin: 10px;
-    padding: 2% 0px;
-    color: white;
-    background-color: green;
-    text-align: center;
+  width: 100%;
+  img {
+    width: 100%;
+    object-fit: cover;
   }
 `;
--- src/components/common/Carousel/Carousel.tsx
+++ src/components/common/Carousel/Carousel.tsx
@@ -1,52 +1,20 @@
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 // extras
 import { CarouselWrapper } from './Carousel.styles';
+import { brands } from '../../../utils/constants';
 
-type Props = {};
-
-function Carousel({}: Props) {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 3,
-  };
-
+function Carousel() {
   return (
     <CarouselWrapper>
-      <Slider {...settings}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-        <div>
-          <h3>7</h3>
-        </div>
-        <div>
-          <h3>8</h3>
-        </div>
-        <div>
-          <h3>9</h3>
-        </div>
-      </Slider>
+      <Swiper spaceBetween={20} slidesPerView={4.3} loop={true}>
+        {brands.map((brand) => (
+          <SwiperSlide key={brand.id}>
+            <img src={`${brand.image}`} alt={brand.title} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
     </CarouselWrapper>
   );
 }
