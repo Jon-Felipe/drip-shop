e56201c5538fb0a65097ead2014e54a97f830bb8
Jon-Felipe <rodriguesjf7@gmail.com>
2025-02-23 13:46:20 +0200

    quantity input component with add and subtract button and quantity value

--- src/components/common/QuantityInput/QuantityInput.tsx
+++ src/components/common/QuantityInput/QuantityInput.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import { FaPlus, FaMinus } from 'react-icons/fa';
 
-type Props = {};
+// extras
+import { QuantityInputWrapper } from './QuantityInput.styles';
 
-function QuantityInput({}: Props) {
-  return <div>QuantityInput</div>;
+export type QuantityInputProps = {
+  quantity: number;
+  onChange: (type: string) => void;
+};
+
+function QuantityInput({ quantity = 0, onChange }: QuantityInputProps) {
+  return (
+    <QuantityInputWrapper>
+      <div>
+        <button className='quantity-button' onClick={() => onChange('minus')}>
+          <FaMinus />
+        </button>
+      </div>
+      <p className='quantity-text'>{quantity}</p>
+      <div>
+        <button className='quantity-button' onClick={() => onChange('plus')}>
+          <FaPlus />
+        </button>
+      </div>
+    </QuantityInputWrapper>
+  );
 }
 
 export default QuantityInput;
